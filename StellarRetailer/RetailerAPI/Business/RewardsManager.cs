50dfc8f61366d57077564d21b28a372ad82c4a00
PratikBompilwar <pratikbompilwar@yahoo.com>
2022-05-19 19:17:45 +0530

    Adding initial draft for API with functionality to calculate rewards point.

---
+++ StellarRetailer/RetailerAPI/Business/RewardsManager.cs
@@ -0,0 +1,25 @@
+﻿namespace RetailerAPI.Business
+{
+    public class RewardsManager 
+    {
+
+        /// <summary>
+        /// Given method returns the Rewards points as per purchase amount
+        /// </summary>
+        /// <param name="purchaseAmount"></param>
+        /// <returns>rewards point</returns>
+        public static int GetRewardsPointsByAmount (int purchaseAmount)
+        {
+            // applying first condition for calculating rewardsPoint
+            int rewardsPoint = purchaseAmount > 50 ? (purchaseAmount - 50) : 0;
+
+            // applying second condition for calculating rewardsPoint
+            if (purchaseAmount > 100)
+            {
+                rewardsPoint += purchaseAmount - 100;
+            }
+
+            return rewardsPoint;
+        }
+    }
+}
---
+++ StellarRetailer/RetailerAPI/Controllers/RewardsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RetailerAPI.Business;
+
+namespace RetailerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class RewardsController : Controller
+    {
+        
+        // GET: api/Rewards/GetRewardPoints/50
+        [Route("GetRewardsPointByAmount/{purchaseAmount}")]
+        [HttpGet]
+        public ActionResult Details(int purchaseAmount)
+        {
+            int result = RewardsManager.GetRewardsPointsByAmount(purchaseAmount);
+
+            return Ok(result);
+        }
+
+        
+    }
+}
